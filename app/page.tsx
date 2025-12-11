ef898799bc677544e24994df2e1678db1c2a7411
Hyewon Lim <hyewon010427@gmail.com>
2025-12-12 01:54:35 +0900

    DeployTest

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
             ©2025 HyewonLim. All rights reserved.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
+            Deploy Test
+            {/* <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
@@ -31,7 +31,7 @@ export default function Home() {
             >
               Learning
             </a>{" "}
-            center.
+            center. */}
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
